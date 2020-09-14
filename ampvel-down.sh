d8830d1bf5ca43771b0ee0a33eb08dd90bf2efd0
Charlie <porthose.cjsmo@gmail.com>
2020-09-13 23:54:39 -0700

    removed not needed files

--- ampvel-down.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker stop Movies-httpd TVShows-httpd;
-cd /home/pi/ampvel && docker-compose down;
-
--- ampvel-up.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker start Movies-httpd TVShows-httpd;
-docker-compose -f /home/pi/ampvel/docker-compose.yml -d --build up;
